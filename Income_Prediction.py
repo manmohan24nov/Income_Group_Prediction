c6965b598e62c9ca5731939e3ea27af53c59491f
Manmohan Singh <manmohan2493singh@yahoo.in>
2016-11-11 16:54:31 +0530

    Update Income_Prediction.py

--- Income_Prediction.py
+++ Income_Prediction.py
@@ -2,13 +2,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
-from statistics  import mode
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
 #Providing required Style
-style.use('ggplot')
+style.use('ggplpot')
 
 #Upload the data for analysis
 train = pd.read_csv('train_.csv')
@@ -18,24 +17,15 @@ test = pd.read_csv('test_.csv')
 test = pd.DataFrame(test)
 
 
-# for i in train:
-#     if i == 'ID':
-#         continue
-#     print(i,':',len(train[i].unique()))
-#     print(train[i].value_counts()/train.shape[0])
-
 #checking missing values
 print(train.apply(lambda x:sum(x.isnull())))
 
 #Filling missing values
 value_to_impute = ['Workclass','Occupation','Native.Country']
 for v in value_to_impute:
-    train[v].fillna(mode(train[v]),inplace=True)
-    test[v].fillna(mode(test[v]),inplace=True)
+    train[v].fillna('-99999',inplace=True)
+    test[v].fillna('-99999',inplace=True)
 
-# var1 = train.query('Sex == "Male"')
-# var2 = var1['Income.Group'].value_counts()
-# print(var2)
 
 #check outlinear
 train.plot('ID','Hours.Per.Week',kind='scatter')
@@ -46,14 +36,14 @@ print(train[var_data_type].apply(lambda x: len(x.unique())))
 
 
 #Modify the data for better performance
-for i in var_data_type:
-    frq = train[i].value_counts()/train.shape[0]
-    category = frq.loc[frq.values <0.05].index
-    for j in category:
-        train[i].replace({j:'Others'},inplace=True)
-        if j == 'Income.Group':
-            continue
-        test[i].replace({j: 'Others'}, inplace=True)
+# for i in var_data_type:
+#     frq = train[i].value_counts()/train.shape[0]
+#     category = frq.loc[frq.values <0.05].index
+#     for j in category:
+#         train[i].replace({j:'Others'},inplace=True)
+#         if j == 'Income.Group':
+#             continue
+#         test[i].replace({j: 'Others'}, inplace=True)
 
 
 #Change the data types of column
@@ -67,19 +57,17 @@ for i in var_data_type1:
 
 dependent_variable = 'Income.Group'
 Independent_variable = [x for x in train.columns if x not in ['ID',dependent_variable]]
-print(Independent_variable)
+
+
 
 #Prediction from given data
-
 model = DecisionTreeClassifier(max_depth=10,min_samples_leaf=100,max_features='sqrt')
 model.fit(train[Independent_variable],train[dependent_variable])
 predict = model.predict(train[Independent_variable])
 predict_test = model.predict(test[Independent_variable])
 
 #Analyse accuracy of Data
-
 acc_train = accuracy_score(train[dependent_variable],predict)
-
 print('Train accuracy : %f'%acc_train)
 
 #Converting data into specific format
@@ -89,9 +77,6 @@ df['ID'] = test['ID']
 df['Income.Group'] = predict_test
 df['Income.Group'] = df['Income.Group'].replace(0,'<=50K')
 df['Income.Group'] = df['Income.Group'].replace(1,'>50K')
-print(df)
 
 #converting data into csv form
 df.to_csv('Solution.csv',index=False)
-
-plt.show()
